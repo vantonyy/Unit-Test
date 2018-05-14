72cfd39335d20266e26a4ff6221a1dbb1afb944d
Vladimir Antonyan <20143682+vantonyy@users.noreply.github.com>
2018-05-15 00:59:40 +0400

    Add files via upload

---
+++ src/common.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+#include <sstream>
+
+namespace common {
+
+class Converter
+{
+public:
+	template <typename ValueType>
+	static std::string toString(const ValueType& value)
+	{
+		std::stringstream ss;
+		ss << value;
+		return ss.str();
+	}
+};
+
+struct CodeLocation
+{
+	CodeLocation(const std::string& a_file, int a_line)
+		: file(a_file)
+		, line(a_line)
+	{}
+
+	std::string file;
+	int line;
+};
+
+} // namespace common
